a521a34d0e3f921d6290594a149ebceb94b898eb
Ava Faghihi <a.faghihi75@gmail.com>
2022-03-12 12:03:32 +0330

    update

--- script/spc-income.js
+++ script/spc-income.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
 	$("#more-box-btn").click(function(){
 		$("#more_box").toggleClass("d-none");
 		if($("#more_box").hasClass("d-none")){
-			$("#more-box-btn").text("مشاهده بیشتر");
+			$("#more-box-btn").html("<i class='fa-solid fa-plus'></i> more box");
 		}else{
-			$("#more-box-btn").text("مشاهده کمتر");
+			$("#more-box-btn").html(" <i class='fa-solid fa-minus mt-2 me-1'></i> less box");
 		}
 	})
 
