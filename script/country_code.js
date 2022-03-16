524200480c3b605eccd88c9d73838d2fb03c484d
Ava Faghihi <a.faghihi75@gmail.com>
2022-03-16 10:12:58 +0330

    country code update

--- script/country_code.js
+++ script/country_code.js
@@ -1,8 +1,8 @@
 // JavaScript Document
 
-     let country_code = document.querySelector("#mobile-number-register-cutomer");
-            window.intlTelInput(country_code, {
-                separateDialCode: true,
-                excludeCountries: ["in", "il"],
-                preferredCountries: ["om", "jp", "pk", "no"]
-            });
+ var input = document.querySelector("#phone");
+	window.intlTelInput(input, {
+		separateDialCode: true,
+		excludeCountries: ["in", "il"],
+		preferredCountries: ["om", "jp", "pk", "no"]
+});
