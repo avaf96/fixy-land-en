a0b1d842fe0f91ef494225caf4071be67d8ec84a
Ava Faghihi <a.faghihi75@gmail.com>
2022-04-11 13:49:45 +0430

    'dar dast ejra' button updates

--- script/spc-desk.js
+++ script/spc-desk.js
@@ -69,7 +69,6 @@ $(document).ready(function(){
 	}
 	
 	
-	
 //	پیشنهادات من
 //	short & long description 
 	
@@ -101,7 +100,7 @@ $(document).ready(function(){
 	}	
 	
 	
-	
+
 	
 //	در دست اجرا
 	//	more info button
@@ -110,23 +109,54 @@ $(document).ready(function(){
 	let job_long_dsc = $(".job-long-dsc");
 	
 	for(let i=0; i<job_more_inf_btn.length; i++){
-		$(job_more_inf_btn[i]).click(function(){
-			$(job_short_dsc[0]).addClass("d-none");
+		let job_more_inf_btn_id = $("#job_more_inf_btn"+[i]);
+		$(job_more_inf_btn_id).click(function(){
+			for(let j=0;j<job_short_dsc.length;j++){
+				$(job_short_dsc[j]).addClass("d-none");
+			}
 			$(job_long_dsc[i]).removeClass("d-none");
 		});
 	}
 	
 	
+	let runing_job_back = $(".runing-job-back");
+	for(let i=0; i<runing_job_back.length; i++){
+		$(runing_job_back[i]).click(function(){
+			for(let j=0;j<job_short_dsc.length;j++){
+				$(job_short_dsc[j]).removeClass("d-none");
+			}
+			$(job_long_dsc[i]).addClass("d-none");
+		});
+	}
+	
+
+	
 	
 //	 صفحه اتمام کار توسط متخصص
-	let paying_btn = $("#paying_btn");
+	let paying_btn = $(".paying_btn");
+	let spc_req_money = $(".spc_req_money");
 	let running_job = $("#running_job");
-	let spc_req_money = $("#spc_req_money");
-	$(paying_btn).click(function(){
-		$(running_job).addClass("d-none");
-		$(spc_req_money).removeClass("d-none");
-	});
+//	let job_short_dsc_paying = $(".job-short-dsc");
+//	let job_long_dsc_paying = $(".job-long-dsc");
+	let spc_req_money_back = $(".spc_req_money_back");
 	
+	for (let i=0 ; i<paying_btn.length; i++){
+		let paying_btn_id = $("#paying_btn"+[i]);
+		$(paying_btn_id).click(function(){
+			$(running_job).addClass("d-none");
+			$(spc_req_money[i]).removeClass("d-none");
+		});
+	}
+	
+	for (let k=0 ; k<spc_req_money_back.length; k++){
+		$(spc_req_money_back[k]).click(function(){
+			$(running_job).removeClass("d-none");
+			$(spc_req_money[k]).addClass("d-none");
+		});
+	}
+	
+	
+
 	
 	
 })
